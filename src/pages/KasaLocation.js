f731f3c9cb95e3b075f3007f5e39d7d38bf35752
youcefKNL <khenichil_youcef@hotmail.fr>
2023-04-22 17:51:28 +0200

    Fin 3e étapes

---
+++ src/pages/KasaLocation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const KasaLocation = () => {
+  return <div></div>;
+};
+
+export default KasaLocation;
---
+++ src/pages/Page404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page404 = () => {
+  return (
+    <div>
+      <h2>page 404</h2>
+    </div>
+  );
+};
+
+export default Page404;
