30b2ffc1c69f41eea85ad3432ba7d42d5fe9f216
youcefKNL <khenichil_youcef@hotmail.fr>
2023-05-02 20:03:33 +0200

    favicon

--- src/components/KasaCards.js
+++ src/components/KasaCards.js
@@ -1,33 +1,3 @@
-// import { useEffect, useState } from "react";
-// import jsonData from "../data/logements.json";
-// //import { NavLink } from "react-router-dom";
-// import KasaCard from "./KasaCard";
-
-// const KasaCards = () => {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         setData(jsonData);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className="kasaCards-content">
-//       {data.map((kasa) => {
-//         return <KasaCard kasa={kasa} key={kasa.id} />;
-//       })}
-//     </div>
-//   );
-// };
-
-// export default KasaCards;
-
 import { useContext } from "react";
 import { DataContext } from "./DataContext";
 import KasaCard from "./KasaCard";
