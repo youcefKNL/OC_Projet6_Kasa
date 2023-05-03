e6b077a723f255340f5ae1c308eeb01ca63d933f
youcefKNL <khenichil_youcef@hotmail.fr>
2023-05-03 17:31:32 +0200

    essai deploiement sur github

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={window.location.pathname || ""}>
       <DataDone>
         <Navigation />
         <Routes>
