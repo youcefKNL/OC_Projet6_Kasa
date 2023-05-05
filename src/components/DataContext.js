d9ec66d55fca1bf7f5e6250c58c27605797d2eca
youcefKNL <khenichil_youcef@hotmail.fr>
2023-05-05 18:00:22 +0200

    mise a jour .gitignore

--- src/components/DataContext.js
+++ src/components/DataContext.js
@@ -1,27 +1,3 @@
-// import React, { createContext, useState, useEffect } from "react";
-// import jsonData from "../data/logements.json";
-
-// export const DataContext = createContext();
-
-// export const DataDone = ({ children }) => {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         setData(jsonData);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <DataContext.Provider value={{ data }}>{children}</DataContext.Provider>
-//   );
-// };
-
 import React, { createContext, useState, useEffect } from "react";
 
 export const DataContext = createContext();
